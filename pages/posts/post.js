1cc022d3a93c23db169bcfc0db7be79a10ca6a70
dp~ <457509824@qq.com>
2017-11-14 15:41:00 +0800

    轮播图

---
+++ pages/posts/post.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    circular:true,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 700
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
