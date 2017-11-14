8a39da40acc00d57499223b96ebf17fec64f254e
dp~ <457509824@qq.com>
2017-11-14 17:42:16 +0800

    添加素材 && 模拟数据

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,22 +1,34 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    imgUrls: [
+    banners: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    circular:true,
+    avatars: [
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+      'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200'
+    ],
+    circular: true,
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 700
   },
-  intervalChange: function (e) {
+  intervalChange(e) {
     this.setData({
       interval: e.detail.value
     })
   },
-  durationChange: function (e) {
+  durationChange(e) {
     this.setData({
       duration: e.detail.value
     })
