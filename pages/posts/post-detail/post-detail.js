6f36f77e31b161f4bc22c11e426892367a52fc96
dp~ <457509824@qq.com>
2017-11-15 18:15:49 +0800

    文章收藏

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -4,12 +4,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    post:{}
+    post: {},
+    postCollected: false
   },
   onLoad(opt) {
     let postId = opt.postId;
     this.setData({
-      post:require('../../../data/posts-data.js')[postId]
+      post: require('../../../data/posts-data.js')[postId]
     })
+    // 获取收藏缓存信息
+    let postsCollected = wx.getStorageSync('postsCollected')
+    if (postsCollected) {
+      if (!postsCollected[postId]) {
+        postsCollected[postId] = false;
+        wx.setStorageSync('postsCollected', postsCollected)
+      }
+      this.setData({
+        postCollected: postsCollected[postId]
+      })
+    } else {
+      postsCollected = {}
+      postsCollected[postId] = false
+      wx.setStorageSync('postsCollected', postsCollected)
+    }
+  },
+  collect() {
+    this.setData({
+      postCollected: !this.data.postCollected
+    })
+    let postsCollected = wx.getStorageSync('postsCollected')
+    postsCollected[this.data.post.postId] = this.data.postCollected
+    wx.setStorageSync('postsCollected', postsCollected)
   }
 })
